2bb38c6cfb62085235f227223a76b2827e267635
Hanif Carroll <HanifCarroll@gmail.com>
2022-06-29 06:50:10 -0500

    Create Note type. Allow note creation functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import './App.css';
+import { FormEvent, useState } from 'react';
+import { Note } from './types';
 
 function App() {
+  const [title, setTitle] = useState('Test Title');
+  const [content, setContent] = useState('Test Content');
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  function createNote(newNote: Note) {
+    setNotes([...notes, newNote]);
+  }
+
+  function clearValues() {
+    setTitle('');
+    setContent('');
+  }
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const newNote: Note = {
+      content,
+      created: new Date(),
+      id: '1',
+      title,
+      updated: new Date(),
+    };
+    createNote(newNote);
+    clearValues();
+  }
+
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Install & Setup Vite + React + Typescript + Tailwind CSS 3
-      </h1>
+      <form onSubmit={onSubmit}>
+        <label>Title</label>
+        <input type={'text'} value={title} onChange={(e) => setTitle(e.target.value)} />
+
+        <label>Content</label>
+        <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+
+        <input type={'submit'} value={'Save'} />
+      </form>
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Note = {
+  content: string;
+  created: Date;
+  id: string;
+  title: string;
+  updated: Date;
+};
